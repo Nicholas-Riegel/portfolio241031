cab4a413ff09ffe523a58a521f48f1bd274edef4
Nicholas Riegel <nicholaspriegel@gmail.com>
2024-11-01 14:41:52 -0400

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className="overflow-x-hidden bg-black tracking-tighter text-gray-200 antialiased">
+      <Navbar />
+    </main>
   )
 }
 
